32d008e40cd6497eecd08b09632f8df15437a049
KangerDrew <kangdrew95@gmail.com>
2021-10-22 13:35:21 -0700

    backend is set up and ready

---
+++ express-back-end/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Express Configuration
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+// Sample GET route
+app.get('/api/data', (req, res) => res.json({
+  message: "Seems to work!",
+}));
+
+app.get('/api/morestuff', (req, res) => res.json({
+  message: "Also works!",
+  moremsg: "stop panicking it works bro"
+}));
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
+});
